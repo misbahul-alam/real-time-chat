b89bdb726b16e143f732b44f7d4983f0a4455720
Misbahul Alam <misbahulalam64@gmail.com>
2025-03-23 16:26:48 +0600

    Add Register Page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/widgets/Button";
 import InputField from "@/components/widgets/InputField";
 import Link from "next/link";
+import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { PiPasswordFill } from "react-icons/pi";
 
@@ -12,10 +13,16 @@ export default function page() {
           <div className="w-full bg-white sm:rounded-lg sm:shadow-sm  md:mt-0 sm:max-w-lg xl:p-0  ">
             <div className="p-6 space-y-4 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
-                Login
+                Create new account
               </h1>
 
               <form className="space-y-1">
+                <InputField
+                  label="Full Name"
+                  type="text"
+                  name="name"
+                  icon={<FaUser />}
+                />
                 <InputField
                   label="Email Address"
                   type="email"
@@ -29,7 +36,12 @@ export default function page() {
                   name="password"
                   icon={<PiPasswordFill />}
                 />
-
+                <InputField
+                  label="Confirm Password"
+                  type="password"
+                  name="password"
+                  icon={<PiPasswordFill />}
+                />
                 <div className="flex items-center justify-between">
                   <div className="flex items-start py-1">
                     <div className="flex items-center h-5 rounded-md overflow-hidden">
@@ -37,34 +49,28 @@ export default function page() {
                         id="terms"
                         aria-describedby="terms"
                         type="checkbox"
-                        className="w-4 h-4 checkbox rounded-md overflow-hidden"
+                        className="w-4 h-4 cursor-pointer"
                       />
                     </div>
-                    <div className="ml-3 text-sm">
+                    <div className="ml-3 text-sm ">
                       <label
                         htmlFor="terms"
                         className="font-light text-gray-500 "
                       >
-                        Remember me
+                        I accept the Terms and Conditions
                       </label>
                     </div>
                   </div>
-                  <Link
-                    href={"/forget"}
-                    className="text-sm font-light text-gray-500"
-                  >
-                    Forget password
-                  </Link>
                 </div>
-                <Button title="LOGIN" className={"w-full"} />
+                <Button title="CREATE" className={"w-full"} />
 
                 <p className="text-sm font-light text-gray-500 ">
-                  I have no account?{" "}
+                  I have an account?{" "}
                   <Link
-                    href="/register"
+                    href="/login"
                     className="font-medium text-primary-600 hover:underline "
                   >
-                    Create an account
+                    Login
                   </Link>
                 </p>
               </form>
